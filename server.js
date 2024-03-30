026fbd3f46890f9fd800384e35899b8be3424460
SATHVIK SVS <svssathvik77@gmail.com>
2024-03-30 12:08:01 +0530

    multer added

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ const cors = require('cors');
 const port = 3000;
 const cloudinary = require('cloudinary').v2;
 const fs = require("fs")
-
-// cloudinary.config({
-//   cloud_name: 'df6o3ijio',
-//   api_key: '156859886331183',
-//   api_secret: '8orEtDydmMM1gfFqbLBGC8VIqUg',
-// //   url:'CLOUDINARY_URL=cloudinary://156859886331183:8orEtDydmMM1gfFqbLBGC8VIqUg@df6o3ijio'
-// });
 const upload = multer({ dest: 'uploads/' });
 app.use(cors());
 
@@ -34,29 +27,9 @@ app.get('/t2v', (req, res) => {
 app.get('/t2i', (req, res) => {
     res.render('t2i.ejs');
 });
-  
-// app.get('/submit', (req, res) => {
-//     const text1 = req.body.text;
-//     const audiofile1 = req.body.audiofile;
-//     console.log(text1,audiofile1);
-// });
-
-app.post('/submit', upload.single('audiofile'), async (req, res) => {
-    const binaryData = req.file.buffer.toString('base64');
-    console.log(req.body,binaryData)
-    try {
-      const response = await axios.post('http://example.com/api', binaryData, {
-        headers: {
-          'Content-Type': 'application/octet-stream',
-        },
-      });
-  
-      res.json({ message: 'File uploaded successfully', response });
-      
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Failed to upload file' });
-    }
+app.post('/submit', upload.single('audiofile'), (req, res) => {
+  console.log(req.file);
+  res.send('File uploaded successfully.');
 });
 
 
