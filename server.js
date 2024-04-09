c156dbdb563d7c037a2cb8c0056be289a4265859
vamsi <vamsikrishnaperuri25@gmail.com>
2024-04-09 13:26:49 +0530

    Model generation

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const port = 3000;
 const cloudinary = require('cloudinary').v2;
 const fs = require("fs")
+const path = require('path');
 const upload = multer({ dest: 'uploads/' });
 app.use(cors());
 
@@ -58,20 +59,68 @@ app.post('/submit', upload.single('audiofile'), async(req, res) => {
     console.log(formData)
     try {
         const response = await axios.post('http://127.0.0.1:5000/upload-audio', formData, {
+            responseType: 'stream',
             headers: {
                 'Content-Type': 'multipart/form-data',
+                
             },
         });
 
+        if (response.status !== 200) {
+            throw new Error(`Error fetching audio: ${response.statusText}`);
+        }
+
+
+        const filename = 'received_audio.wav'; // Adjust filename as needed
+
+        const writeStream = fs.createWriteStream(filename);
+        response.data.pipe(writeStream); // Pipe the audio stream to the file
+
+        writeStream.on('finish', () => {
+            console.log('Audio file received successfully.');
+            // res.send('Audio received!'); // Send confirmation response to the Flask server
+        });
+
+        writeStream.on('error', (error) => {
+            console.error('Error saving audio file:', error);
+            res.status(500).send('Failed to receive audio.'); // Send error response
+        });
+        console.log("upload success to local storage");
+
+        // const downloadFilePath = ('./received_audio.wav');
+
+        // res.sendFile(downloadFilePath, (err) => {
+        //     if (err) {
+        //     console.error(err);
+        //     res.status(500).send('Error downloading file');
+        //     } else {
+        //     console.log('File downloaded successfully');
+        //     }
+        // });
+        
         res.send('File uploaded successfully.'+response.data);
-        console.log("upload success to flask");
+        console.log("try executed");
+        
     } catch (error) {
         console.error(error.message);
         res.status(500).send('Failed to upload file.');
     }
+
+
 });
 
+// const downloadFilePath = './received_audio.wav';
 
+// app.get('/download', (req, res) => {
+//   res.sendFile(downloadFilePath, (err) => {
+//     if (err) {
+//       console.error(err);
+//       res.status(500).send('Error downloading file');
+//     } else {
+//       console.log('File downloaded successfully');
+//     }
+//   });
+// });
 
 app.get('/t2isubmit', (req, res) => {
     res.render('t2isubmit',{
